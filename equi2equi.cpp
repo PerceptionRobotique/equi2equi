2482c62a5c525c6f634bf7bca944b442c328afac
Guillaume Caron <guillaume.caron@u-picardie.fr>
2023-01-31 09:31:13 +0100

    add the possibility to transform equirectangular RGBD images, with a forward transformation

--- equi2equi.cpp
+++ equi2equi.cpp
@@ -4,6 +4,8 @@
  * example of command line :
 
   ./equi2equi ../media/ 0 9 1 ../media/poses.txt 1
+or
+
 
  \param imagesDir directory where equirectangular images to read (with 6 digits before the extension) are and where the output equirectangular images will be written (with characters 'e_' before the 6 digits)
  \param iFirst the number of the first image to transform
@@ -31,6 +33,7 @@
 // VISP includes
 #include <visp/vpImage.h>
 #include <visp/vpImageIo.h>
+#include <visp/vpImageTools.h>
 
 #include <visp/vpTime.h>
 
@@ -38,7 +41,7 @@
 
 #define INTERPTYPE prInterpType::IMAGEPLANE_BILINEAR
 
-//#define VERBOSE
+#define VERBOSE
 
 /*!
  * \fn main()
@@ -142,11 +145,32 @@ int main(int argc, char **argv)
         
     //Add depthmap and only in this case the translations of v_pv_init will not be ignored
     bool depthSoForwardTransf = false;
+		//Loading the depthmap of the reference image with respect to which the cost function will be computed
+    vpImage<float> depthmap_se, depthmap_te;
+    char *depthmapPath = NULL;
+    if(argc < 8)
+    {
+#ifdef VERBOSE
+        std::cout << "no depthmap files directory path given" << std::endl;
+#endif
+        
+    }
+    else
+    {
+    	depthmapPath = (char *)argv[7];
+    	depthSoForwardTransf = true;
+    }
+
+    //Get filename thanks to boost
+    char myFilter_depth[1024];
+		char ext_depthmap[] = "pfm";
     
 
     vpDisplayX disp;
     vpDisplayX disp2;
+    vpDisplayX disp_depthmap;
     
+    vpImage<unsigned char> depthmap_se_disp;
     
     //Pour chaque image du dataset
     int nbPass = 0;
@@ -170,11 +194,16 @@ int main(int argc, char **argv)
     filename = s.str();
     std::ofstream ficPosesInit_updt(filename);
     
-    vpImage<vpRGBa> I_se; //Dual fisheye image
-    vpImage<vpRGBa> I_te; //Equirectangular image
+    vpImage<vpRGBa> I_se; //Source Equirectangular image
+    vpImage<vpRGBa> I_te; //Transformed Equirectangular image
     
     boost::filesystem::path dir(chemin);
     boost::regex my_filter;
+		boost::filesystem::path dir_depthmap;
+		if(depthSoForwardTransf)
+			dir_depthmap=boost::filesystem::path(depthmapPath);
+			
+		boost::regex my_filter_depthmap;
     std::string name;
     
     prEquirectangular equirectCam;
@@ -182,7 +211,8 @@ int main(int argc, char **argv)
     double Xs, Ys, Zs, u, v, dv, du, unmdv, unmdu;
     unsigned int icam, imWidth, imHeight;
     unsigned int i, j;
-    vpRGBa curPix, *pt_bitmap_te;
+    vpRGBa curPix, *pt_bitmap_te, *pt_bitmap_se;
+    float *pt_depthmap_se;
     while(!clickOut && (imNum <= i360))
     {
         temps = vpTime::measureTimeMs();
@@ -217,6 +247,46 @@ int main(int argc, char **argv)
         vpDisplay::display(I_se);
         vpDisplay::flush(I_se);
         
+        if(depthSoForwardTransf)
+        {
+					//Load and display depthmap_req
+					sprintf(myFilter_depth, "%06d.*\\.%s", imNum, ext_depthmap); //filter on extension not working
+
+					my_filter_depthmap.set_expression(myFilter_depth);
+
+					for (boost::filesystem::directory_iterator iter(dir_depthmap),end; iter!=end; ++iter)
+					{
+						name = iter->path().filename().string();
+						std::cout << iter->path().string() << std::endl;
+						if (boost::regex_match(name, my_filter_depthmap))
+						{
+							vpImageIo::readPFM(depthmap_se, iter->path().string());
+							break;
+						}
+					}
+
+					//std::cout << depthmap_se.getWidth() << " " << depthmap_se.getHeight() << std::endl;
+
+					if( (depthmap_se.getWidth() != I_se.getWidth()) || (depthmap_se.getHeight() != I_se.getHeight()) )
+					{
+						vpImage<float> depthmap_se_resize;
+						vpImageTools::resize(depthmap_se, depthmap_se_resize, I_se.getWidth(), I_se.getHeight(), vpImageTools::INTERPOLATION_LINEAR);//vpImageTools::INTERPOLATION_AREA);
+						depthmap_se = depthmap_se_resize;
+					}
+
+					//std::cout << depthmap_se.getWidth() << " " << depthmap_se.getHeight() << std::endl;
+
+					vpImageConvert::convert(depthmap_se, depthmap_se_disp); 	
+					
+        	if(nbPass == 0)
+        	{
+						disp_depthmap.init(depthmap_se_disp, 250, 25, "depthmap_se");
+					}
+					
+					vpDisplay::display(depthmap_se_disp);
+					vpDisplay::flush(depthmap_se_disp);
+        }
+        
         if(v_pv_init.size() > imNum)
         {
         		if(depthSoForwardTransf)
@@ -239,71 +309,116 @@ int main(int argc, char **argv)
         vpPoseVector p_updt(cMc0);
         ficPosesInit_updt << p_updt.t() << std::endl;
         
-      
-        //equirectangular to equirectangular (no depth, inverse mapping)
-        pt_bitmap_te = I_te.bitmap;
-        for(unsigned int v_te = 0 ; v_te < imHeight ; v_te++)
+        if(!depthSoForwardTransf)
         {
-            for(unsigned int u_te = 0 ; u_te < imWidth ; u_te++, pt_bitmap_te++)
-            {
-                //Equirectangular to sphere
-                P.set_u(u_te);
-                P.set_v(v_te);
-                equirectCam.pixelMeterConversion(P);
-                equirectCam.projectImageSphere(P, Xs, Ys, Zs);
-                //sphere rotation
-                P.set_oX(Xs);
-                P.set_oY(Ys);
-                P.set_oZ(Zs);
-                
-                //rotation "compensation"
-                P.changeFrame(cMc0);
-                
-                //sphere to equirectangular
-                equirectCam.project3DImage(P);
-                equirectCam.meterPixelConversion(P);
-                                 
-                u = P.get_u();
-                v = P.get_v();
-                
-                if( (u >= 0) && (v >= 0) && (u < (imWidth-1)) && (v < (imHeight-1))  )
-                {
-                    switch(INTERPTYPE)
-                    {
-                        case IMAGEPLANE_BILINEAR:
-                            curPix.R = curPix.G = curPix.B = 0;
-                            
-                            i = (int)v; dv = v-i; unmdv = 1.0-dv;
-                            j = (int)u; du = u-j; unmdu = 1.0-du;
-                            
-                            
-                                curPix = curPix + I_se[i][j]*unmdv*unmdu;
+		      //equirectangular to equirectangular (no depth, inverse mapping)
+		      pt_bitmap_te = I_te.bitmap;
+		      for(unsigned int v_te = 0 ; v_te < imHeight ; v_te++)
+		      {
+		          for(unsigned int u_te = 0 ; u_te < imWidth ; u_te++, pt_bitmap_te++)
+		          {
+		              //Equirectangular to sphere
+		              P.set_u(u_te);
+		              P.set_v(v_te);
+		              equirectCam.pixelMeterConversion(P);
+		              equirectCam.projectImageSphere(P, Xs, Ys, Zs);
+		              //sphere rotation
+		              P.set_oX(Xs);
+		              P.set_oY(Ys);
+		              P.set_oZ(Zs);
+		              
+		              //rotation "compensation"
+		              P.changeFrame(cMc0);
+		              
+		              //sphere to equirectangular
+		              equirectCam.project3DImage(P);
+		              equirectCam.meterPixelConversion(P);
+		                               
+		              u = P.get_u();
+		              v = P.get_v();
+		              
+		              if( (u >= 0) && (v >= 0) && (u < (imWidth-1)) && (v < (imHeight-1))  )
+		              {
+		                  switch(INTERPTYPE)
+		                  {
+		                      case IMAGEPLANE_BILINEAR:
+		                          curPix.R = curPix.G = curPix.B = 0;
+		                          
+		                          i = (int)v; dv = v-i; unmdv = 1.0-dv;
+		                          j = (int)u; du = u-j; unmdu = 1.0-du;
+		                          
+		                          
+		                              curPix = curPix + I_se[i][j]*unmdv*unmdu;
 
-                                curPix = curPix + I_se[i+1][j]*dv*unmdu;
-                            
-                                curPix = curPix + I_se[i][j+1]*unmdv*du;
-                            
-                                curPix = curPix + I_se[i+1][j+1]*dv*du;
-                            
-                            
-                            *pt_bitmap_te = curPix;
-                            
-                            break;
-                        case IMAGEPLANE_NEARESTNEIGH:
-                        default:
-                            
-                            i = vpMath::round(v);
-                            j = vpMath::round(u);
-                            
-                            
-                                *pt_bitmap_te = I_se[i][j];
-                            
-                            
-                            break;
-                    }
-                }
-            }
-            //std::cout << P.get_u() << "\t" << P.get_v() << "\t" << P.get_x() << "\t" << P.get_y() << std::endl;
+		                              curPix = curPix + I_se[i+1][j]*dv*unmdu;
+		                          
+		                              curPix = curPix + I_se[i][j+1]*unmdv*du;
+		                          
+		                              curPix = curPix + I_se[i+1][j+1]*dv*du;
+		                          
+		                          
+		                          *pt_bitmap_te = curPix;
+		                          
+		                          break;
+		                      case IMAGEPLANE_NEARESTNEIGH:
+		                      default:
+		                          
+		                          i = vpMath::round(v);
+		                          j = vpMath::round(u);
+		                          
+		                          
+		                              *pt_bitmap_te = I_se[i][j];
+		                          
+		                          
+		                          break;
+		                  }
+		              }
+		          }
+		          //std::cout << P.get_u() << "\t" << P.get_v() << "\t" << P.get_x() << "\t" << P.get_y() << std::endl;
+		      }
+        }
+        else
+        {
+        	//equirectangular to equirectangular (depth so forward mapping)
+		      pt_bitmap_se = I_se.bitmap;
+		      pt_depthmap_se = depthmap_se.bitmap;
+		      for(unsigned int v_se = 0 ; v_se < imHeight ; v_se++)
+		      {
+		          for(unsigned int u_se = 0 ; u_se < imWidth ; u_se++, pt_bitmap_se++, pt_depthmap_se++)
+		          {
+		          	if(*pt_depthmap_se > 0)
+		          	{
+		              //Equirectangular to sphere
+		              P.set_u(u_se);
+		              P.set_v(v_se);
+		              equirectCam.pixelMeterConversion(P);
+		              equirectCam.projectImageSphere(P, Xs, Ys, Zs);
+		              //sphere rotation
+		              P.set_oX(Xs*(*pt_depthmap_se));
+		              P.set_oY(Ys*(*pt_depthmap_se));
+		              P.set_oZ(Zs*(*pt_depthmap_se));
+		              
+		              //rotation "compensation"
+		              P.changeFrame(cMc0.inverse());
+		              
+		              //sphere to equirectangular
+		              equirectCam.project3DImage(P);
+		              equirectCam.meterPixelConversion(P);
+		                               
+		              u = P.get_u();
+		              v = P.get_v();
+		              
+		              if( (u >= 0) && (v >= 0) && (u < (imWidth-1)) && (v < (imHeight-1))  )
+		              {                       
+                      i = vpMath::round(v);
+                      j = vpMath::round(u);
+                      
+                      I_te[i][j] = *pt_bitmap_se;           
+		              }
+		            }
+		          }
+		          //std::cout << P.get_u() << "\t" << P.get_v() << "\t" << P.get_x() << "\t" << P.get_y() << std::endl;
+		      }
         }
         
         v_temps.push_back(vpTime::measureTimeMs()-temps);
